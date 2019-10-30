9dab1458f191649e25ebd24b46ac16182737bfe0
Sanjeev Kumar <sanjeev.kumar59@ad.infosys.com>
2019-10-31 02:30:47 +0530

    Views are complete; Routing is OK; empty templates are in place

--- PyBlog/blog/urls.py
+++ PyBlog/blog/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path("post/<pk>/edit/", views.PostUpdateView.as_view(), name="post_edit"),
     path("post/<pk>/delete/", views.PostDeleteView.as_view(), name="post_delete"),
     path("drafts/", views.DraftListView.as_view(), name="post_drafts"),
+    path("post/<pk>/comment", views.add_comment_to_post, name="add_comment_to_post"),
+    path("comments/<pk>/approve", views.comment_approve, name="comment_approve"),
+    path("comments/<pk>/remove", views.comment_remove, name="comment_remove"),
+    path("post/<pk>/publish", views.post_publish, name="post_publish"),
 ]
 
--- PyBlog/blog/views.py
+++ PyBlog/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import (
     TemplateView,
     ListView,
@@ -10,6 +10,7 @@ from django.views.generic import (
 from django.urls import reverse_lazy
 from blog import models, forms
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -24,9 +25,9 @@ class PostListView(ListView):
     model = models.Post
 
     def get_queryset(self):
-        return models.Post.objects.filter(
-            published_date__lte=timezone.now()).order_by("-published_date")
-        
+        return models.Post.objects.filter(published_date__lte=timezone.now()).order_by(
+            "-published_date"
+        )
 
 
 class PostDetaiilView(DetailView):
@@ -61,3 +62,38 @@ class DraftListView(LoginRequiredMixin, ListView):
         return models.Post.objects.filter(published_date__isnull=True).order_by(
             "created_date"
         )
+
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(models.Post, pk=pk)
+    if request.method == "POST":
+        form = forms.CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect("post_detail", pk=post.pk)
+    else:
+        form = forms.CommentForm()
+    return render(request, "blog/comment_form,html", {"form": form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(models.Comments, pk=pk)
+    comment.approve()
+    return redirect("post_detail", pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(models.Comments, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect("post_detail", pk=post_pk)
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(models.Post, pk=pk)
+    post.publish()
+    return redirect("post_detail", pk=post.pk)
