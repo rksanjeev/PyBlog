f81451f5920dd4083152b465804cc63bca317c9d
Sanjeev Kumar <sanjeev.kumar59@ad.infosys.com>
2019-10-31 23:15:58 +0530

    login and logout url path

--- PyBlog/PyBlog/urls.py
+++ PyBlog/PyBlog/urls.py
@@ -15,6 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include('blog.urls'))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls")),
+    path("accounts/login/", views.LoginView, name="login"),
+    path(
+        "accounts/logout/", views.LogoutView, name="logout", kwargs={"next_page": "/"}
+    ),
+]
 
